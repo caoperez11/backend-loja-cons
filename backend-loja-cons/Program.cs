37cb2d0fdb0c2f17b4bbfe151b957b3ce0dbcf8e
Alejandro Perez <caperezdev11@gmail.com>
2022-07-15 07:19:35 -0500

    Correciones

--- backend-loja-cons/Program.cs
+++ backend-loja-cons/Program.cs
@@ -9,10 +9,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    //app.UseSwagger();
     app.UseSwaggerUI();
 }
 
